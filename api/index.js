943d65eab645822da67f69e5ab25433705590f20
danielforkner <daniel.forkner@gmail.com>
2022-07-16 11:59:37 -0400

    import bug

--- api/index.js
+++ api/index.js
@@ -1,32 +1,32 @@
-const apiRouter = require("express").Router();
-const usersRouter = require("./users");
-const customersRouter = require("./customers");
-const { getUserByEmail } = require("../db/models/users");
+const apiRouter = require('express').Router();
+const usersRouter = require('./users');
+const customersRouter = require('./customers');
+const { getUserByEmail } = require('../db/models/users');
 
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = process.env;
 
 apiRouter.use(async (req, res, next) => {
-  console.log("Checking for authorization...");
+  console.log('Checking for authorization...');
   const prefix = `Bearer `;
-  const auth = req.header("Authorization");
+  const auth = req.header('Authorization');
   if (!auth) {
-    console.log("No auth provided. Continuing.");
+    console.log('No auth provided. Continuing.');
     next();
   } else if (auth.startsWith(prefix)) {
     const token = auth.slice(prefix.length);
     try {
       const { email } = jwt.verify(token, JWT_SECRET);
       if (email) {
-        console.log("Good token. Setting user.");
-        user = await getUserByEmail(email);
+        console.log('Good token. Setting user.');
+        let user = await getUserByEmail(email);
         delete user.password;
         req.user = user;
         console.log("WHO'S THE USER IN API", user);
         next();
       } else {
         res.status(409);
-        next({ name: "BadTokenError", message: "Invalid Token" });
+        next({ name: 'BadTokenError', message: 'Invalid Token' });
       }
     } catch ({ name, message }) {
       next({ name, message });
@@ -34,29 +34,29 @@ apiRouter.use(async (req, res, next) => {
   } else {
     res.status(409);
     next({
-      name: "AuthorizationHeaderError",
+      name: 'AuthorizationHeaderError',
       message: `Authorization token must start with ${prefix}`,
     });
   }
 });
 
-apiRouter.get("/", (req, res, next) => {
+apiRouter.get('/', (req, res, next) => {
   res.send({
-    message: "specify an api route to proceed",
+    message: 'specify an api route to proceed',
   });
 });
 
-apiRouter.get("/health", (req, res, next) => {
+apiRouter.get('/health', (req, res, next) => {
   res.send({
     healthy: true,
   });
 });
 
-apiRouter.use("/users", usersRouter);
-apiRouter.use("/customers", customersRouter);
+apiRouter.use('/users', usersRouter);
+apiRouter.use('/customers', customersRouter);
 
 apiRouter.use((error, req, res, next) => {
-  console.log("SENDING ERROR: ", error);
+  console.log('SENDING ERROR: ', error);
   res.send(error);
 });
 
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //This is the Web Server
 
-import dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import path from "path";
-import client from "./db/client.js";
-import apiRouter from "./api/index.js";
-import { fileURLToPath } from "url";
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
+import client from './db/client.js';
+import apiRouter from './api/index.js';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
@@ -21,28 +21,28 @@ const server = express();
 server.use(cors());
 
 // create logs for everything
-server.use(morgan("dev"));
+server.use(morgan('dev'));
 
 //handles application/json requests
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 server.use((req, res, next) => {
-  console.log("BODY LOGGER START");
+  console.log('BODY LOGGER START');
   console.log(req.body);
-  console.log("BODY LOGGER END");
+  console.log('BODY LOGGER END');
   next();
 });
 
 //here's our static files
 
-server.use(express.static(path.join(__dirname, "build")));
+server.use(express.static(path.join(__dirname, 'build')));
 
 //here's the API
 server.use(apiRouter);
 // server.use("/api", require("./api"));
 
 server.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 //bring in the DB connection
@@ -56,8 +56,8 @@ export const handle = server.listen(PORT, async () => {
   console.log(`Server is running on ${PORT}!`);
   try {
     await client.connect();
-    console.log("Database is open for business!");
+    console.log('Database is open for business!');
   } catch (error) {
-    console.error("Database is closed for repairs!\n", error);
+    console.error('Database is closed for repairs!\n', error);
   }
 });
